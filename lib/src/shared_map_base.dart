332b52b3b43cdb226a7d2384f717afde8279b7f0
gmpassos <gracilianomp@gmail.com>
2023-11-17 05:20:54 -0300

    v1.0.1 - Add documentation.

--- lib/src/shared_map_base.dart
+++ lib/src/shared_map_base.dart
@@ -3,55 +3,75 @@ import 'dart:async';
 import 'shared_map_generic.dart'
     if (dart.library.isolate) 'shared_map_isolate.dart';
 
+/// Base class for [SharedStore] and [SharedMap] implementations.
 abstract class SharedType {
+  /// The ID of the shared instance.
   String get id;
 
+  /// Returns the [SharedReference] of this instances, to instantiated it
+  /// using `fromSharedReference` constructor.
   SharedReference shareReference();
 }
 
+/// Base class for [SharedStore] implementations.
 abstract class SharedStore extends SharedType {
+  /// Creates a [SharedStore] with [id].
   factory SharedStore(String id) {
     return createSharedStore(id: id);
   }
 
+  /// Creates a [SharedStore] from [sharedReference].
   factory SharedStore.fromSharedReference(
       SharedStoreReference sharedReference) {
     return createSharedStore(sharedReference: sharedReference);
   }
 
+  /// Returns a [SharedMap] with [id] in this [SharedStore] instance.
   FutureOr<SharedMap<K, V>?> getSharedMap<K, V>(String id);
 
   @override
   SharedStoreReference shareReference();
 }
 
+/// Base class for [SharedMap] implementations.
 abstract class SharedMap<K, V> extends SharedType {
+  /// Creates a [SharedMap] with [id].
   factory SharedMap(SharedStore sharedStore, String id) {
     return createSharedMap(sharedStore: sharedStore, id: id);
   }
 
+  /// Creates a [SharedMap] from [sharedReference].
   factory SharedMap.fromSharedReference(SharedMapReference sharedReference) {
     return createSharedMap(sharedReference: sharedReference);
   }
 
+  /// The [SharedStore] where this instance is stored/handled.
   SharedStore get sharedStore;
 
+  /// Returns the value of [key].
   FutureOr<V?> get(K key);
 
+  /// Sets the [value] of [key] and returns it.
   FutureOr<V?> put(K key, V? value);
 
   @override
   SharedMapReference shareReference();
 }
 
+/// Base class for [SharedReference] implementations.
 abstract class SharedReference {
+  /// The ID of the referenced instance.
+  final String id;
+
+  SharedReference(this.id);
+
+  /// The JSON of this [SharedReference].
   Map<String, dynamic> toJson();
 }
 
+/// Shared reference to a [SharedStore].
 class SharedStoreReference extends SharedReference {
-  final String id;
-
-  SharedStoreReference(this.id);
+  SharedStoreReference(super.id);
 
   factory SharedStoreReference.fromJson(Map<String, dynamic> json) {
     return SharedStoreReference(json['id']);
@@ -64,11 +84,12 @@ class SharedStoreReference extends SharedReference {
   String toString() => 'SharedStoreReference${toJson()}';
 }
 
+/// Shared reference to a [SharedMap].
 class SharedMapReference extends SharedReference {
-  final String id;
+  /// The [SharedStoreReference] of the [SharedStore] of the referenced [SharedMap].
   final SharedStoreReference sharedStoreReference;
 
-  SharedMapReference(this.id, this.sharedStoreReference);
+  SharedMapReference(super.id, this.sharedStoreReference);
 
   factory SharedMapReference.fromJson(Map<String, dynamic> json) {
     var sharedStoreReference =
